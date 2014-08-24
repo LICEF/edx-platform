5ebc3355365df430b63961106314f2f078be7e85
MOHAMED ben jemia <bondak@gmail.com>
2014-08-23 23:22:44 -0400

    wip

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1325,7 +1325,7 @@ MKTG_URL_LINK_MAP = {
     'APROPOS': 'about_edx',
     'CONTACT': 'contact',
     'FAQ': 'help_edx',
-    'COURSES': 'courses',
+    'COURS': 'courses',
     'ROOT': 'root',
     'TOS': 'tos',
     'HONOR': 'honor',
