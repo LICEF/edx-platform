8bf63ea10f57c4d0eaa9d2152ef5db2899fde762
MOHAMED ben jemia <bondak@gmail.com>
2014-10-19 16:39:54 -0400

    translation

--- common/lib/xmodule/xmodule/capa_base.py
+++ common/lib/xmodule/xmodule/capa_base.py
@@ -1,3 +1,6 @@
+
+# * coding: utf-8
+
 """Implements basics of Capa, including class CapaModule."""
 import cgi
 import copy
@@ -1344,7 +1347,7 @@ class CapaMixin(CapaFields):
         self.track_function_unmask('save_problem_success', event_info)
         msg = _("Your answers have been saved.")
         if not self.max_attempts == 0:
-            msg = _("Your answers have been saved but not graded. Click 'Check' to grade them.")
+            msg = u"Vos réponses ont été enregistrées mais pas encore évaluées. Cliquez sur 'Valider' pour les évaluer."
         return {
             'success': True,
             'msg': msg,
